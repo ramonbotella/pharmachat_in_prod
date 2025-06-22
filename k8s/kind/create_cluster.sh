b94e0d51c37606895283cf0401c244b0dd8181cf
Ramon Botella Nieto <rbnieto@gmail.com>
2025-06-22 13:38:05 +0200

    Add scripts for creating and managing Kubernetes cluster with Docker

---
+++ k8s/kind/create_cluster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Steps:
+
+# 1. Delete the cluster (if it exists, otherwise it will fail)
+echo "Deleting the cluster..."
+kind delete cluster --name pharma-cluster
+
+# 2. Delete the docker network (if it exists, otherwise it will fail)
+echo "Deleting the docker network..."
+docker network rm pharma-network
+
+# 3. Create the docker network
+echo "Creating the docker network..."
+docker network create --subnet 172.100.0.0/16 pharma-network
+
+# 4. Create the cluster
+echo "Creating the cluster..."
+KIND_EXPERIMENTAL_DOCKER_NETWORK=pharma-network kind create cluster --config ./kind-with-portmapping.yaml
